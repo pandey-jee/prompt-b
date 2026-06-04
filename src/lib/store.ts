74b1a281a60e73b579efb9a598b1726e39f4fb81
Abhishek <136082512+pandey-jee@users.noreply.github.com>
2026-06-04 10:42:38 +0530

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -10,7 +10,7 @@ import {
 } from "./persistence";
 import { PlayerSubmission } from "./types";
 
-const CHALLENGE_ROTATION_MS = 10 * 60 * 1000;
+const CHALLENGE_ROTATION_MS = 20 * 60 * 1000;
 const SESSION_WINDOW_MS = 20 * 60 * 1000;
 
 declare global {
